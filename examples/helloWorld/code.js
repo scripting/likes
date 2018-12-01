5c76df4da41cd7ce56250b5767ac51e5e14979a7
Dave Winer <dave.winer@gmail.com>
2018-12-01 12:52:28 -0500

    v0.4.12

--- examples/helloWorld/code.js
+++ examples/helloWorld/code.js
@@ -1,24 +1,5 @@
-var appConsts = {
-	domain: "littleoutliner.com",
-	version: "0.4.0"
-	};
-function everyMinute () {
-	var now = new Date ();
-	console.log ("\neveryMinute: " + now.toLocaleTimeString () + ", v" + appConsts.version);
-	}
-function everySecond () {
-	}
 function startup () {
 	console.log ("startup");
-	$(".likeable").each (function () {
-		$(this).attr ("urlForLike", window.location.href);
-		});
-	var options = {
-		urlLikesServer: "http://likes.scripting.com/"
-		};
-	myNodeLikesApp = new nodeLikesApp (options, function () {
-		hitCounter (); 
-		initGoogleAnalytics (); 
-		self.setInterval (everySecond, 1000); 
+	myNodeLikesApp = new nodeLikesApp (undefined, function () {
 		});
 	}

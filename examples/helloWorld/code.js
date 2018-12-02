fc5d88be9031b9ace27736fe320213832d78616a
Dave Winer <dave.winer@gmail.com>
2018-12-02 10:57:29 -0500

    v0.4.12

--- examples/helloWorld/code.js
+++ examples/helloWorld/code.js
@@ -2,6 +2,13 @@ var myNodeLikesApp;
 
 function startup () {
 	console.log ("startup");
-	myNodeLikesApp = new nodeLikesApp (undefined, function () {
+	
+	var options = {
+		flPrepend: false,
+		addTheWordLikes: false
+		};
+	
+	myNodeLikesApp = new nodeLikesApp (options, function () {
+		console.log ("startup: all the likes have been set up.");
 		});
 	}
